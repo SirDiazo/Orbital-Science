5cf7ace765e75ea6b0996aba1e76da5377d8a056
David Grandy <david.grandy@gmail.com>
2014-05-08 21:02:26 -0400

    Don't need to watch for transmission here

--- Source/DMScienceWatcher.cs
+++ Source/DMScienceWatcher.cs
@@ -9,12 +9,17 @@ namespace DMagic
     [KSPAddonFixed(KSPAddon.Startup.MainMenu, true, typeof(DMScienceWatcher))]
     internal class DMScienceWatcher: MonoBehaviour
     {
-        private List<IScienceDataTransmitter> tranList = new List<IScienceDataTransmitter>();
 
         private void Start()
         {
             GameEvents.OnVesselRecoveryRequested.Add(RecoveryWatcher);
             GameEvents.onFlightReady.Add(SciScenarioStarter);
+        }
+
+        private void OnDestroy()
+        {
+            GameEvents.OnVesselRecoveryRequested.Remove(RecoveryWatcher);
+            GameEvents.onFlightReady.Remove(SciScenarioStarter);
         }
 
         private void SciScenarioStarter()
@@ -42,47 +47,5 @@ namespace DMagic
             }
         }
 
-        private void TransmissionWatcher()
-        {
-            Vessel v = FlightGlobals.ActiveVessel;
-            tranList = v.FindPartModulesImplementing<IScienceDataTransmitter>();
-            List<DMScienceScenario.DMScienceData> DMdataList = new List<DMScienceScenario.DMScienceData>();
-            List<ScienceData> relevantDataList = new List<ScienceData>();
-            foreach (IScienceDataTransmitter transmitter in tranList)
-            {
-                if (transmitter.IsBusy())
-                {
-                    List<ScienceData> dataList = new List<ScienceData>();
-                    foreach (IScienceDataContainer cont in v.FindPartModulesImplementing<IScienceDataContainer>())
-                    {
-                        dataList.AddRange(cont.GetData());
-                    }
-                    if (dataList.Count > 0)
-                    {
-                        foreach (ScienceData data in dataList)
-                        {
-                            foreach (DMScienceScenario.DMScienceData DMData in DMScienceScenario.recoveredScienceList)
-                            {
-                                if (data.subjectID == DMData.id)
-                                {
-                                    relevantDataList.Add(data);
-                                    DMdataList.Add(DMData);
-                                }
-                            }
-                        }
-                    }
-                        
-                        
-                        
-                        
-                    
-                }
-            }
-        }
-
-        private void OnDestroy()
-        {
-            GameEvents.OnVesselRecoveryRequested.Remove(RecoveryWatcher);
-        }
     }
 }
