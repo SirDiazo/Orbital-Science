f76bda8c7c6253f75ba1d96697176491f910d212
DMagic <david.grandy@gmail.com>
2014-05-12 23:02:17 -0400

    Successfully recover data from onVesselRecovered event

--- Source/DMScienceWatcher.cs
+++ Source/DMScienceWatcher.cs
@@ -10,12 +10,27 @@ namespace DMagic
     internal class DMScienceWatcher: MonoBehaviour
     {
         protected MagicDataTransmitter magicTransmitter;
+        protected List<ScienceData> dataList = new List<ScienceData>();
 
         private void Start()
         {
             GameEvents.OnVesselRecoveryRequested.Add(RecoveryWatcher);
+            GameEvents.onVesselRecovered.Add(ProtoRecoveryWatcher);
             GameEvents.onFlightReady.Add(SciScenarioStarter);
+
+
+            //When the vessel is actually ready to control, after onInitialize?
+            GameEvents.onFlightReady.Add(flightready);
+            //When the vessel is first loaded
+            GameEvents.onVesselLoaded.Add(vesselload);
+            //When vessel is changed, applies on load
+            GameEvents.onVesselChange.Add(vesselchange);
+            //triggered when recovered from in-flight
+            GameEvents.OnVesselRecoveryRequested.Add(recoveryrequest);
+            //When vessel is recovered from tracking center/spacecenter, also triggered after recovery requested
+            GameEvents.onVesselRecovered.Add(vesselrecovered);
         }
+
 
         private void OnDestroy()
         {
@@ -28,9 +43,39 @@ namespace DMagic
             //DMScienceScenario Scenario = DMScienceScenario.SciScenario;
         }
 
+        private void vesselrecovered(ProtoVessel v)
+        {
+            EventDebug("vessel recover");
+        }
+
+        private void recoveryrequest(Vessel v)
+        {
+            EventDebug("recovery request");
+        }
+
+        private void vesselchange(Vessel v)
+        {
+            EventDebug("vessel change");
+        }
+
+        private void vesselload(Vessel v)
+        {
+            EventDebug("vessel load");
+        }
+
+        private void flightready()
+        {
+            EventDebug("flight ready");
+        }
+
+        private void EventDebug(string gevent)
+        {
+            print("GameEvent " + gevent + " triggered");
+        }
+
         private void RecoveryWatcher(Vessel v)
         {
-            List<ScienceData> dataList = new List<ScienceData>();
+            dataList.Clear();
             foreach (IScienceDataContainer cont in v.FindPartModulesImplementing<IScienceDataContainer>())
             {
                 dataList.AddRange(cont.GetData());
@@ -48,14 +93,46 @@ namespace DMagic
             }
         }
 
-        private void TransmissionWatcher(Vessel v)
+        private void ProtoRecoveryWatcher(ProtoVessel v)
         {
-            List<ScienceData> dataList = new List<ScienceData>();
+            EventDebug(v.vesselName);
+            foreach (ProtoPartSnapshot snap in v.protoPartSnapshots)
+            {
+                foreach (ProtoPartModuleSnapshot msnap in snap.modules)
+                {
+                    foreach (ConfigNode dataNode in msnap.moduleValues.GetNodes("ScienceData"))
+                    {
+                        string id = dataNode.GetValue("subjectID");
+                        print("Found science data with subject ID: " + id);
+                        ScienceSubject sub = ResearchAndDevelopment.GetSubjectByID(id);
+                        if (sub != null)
+                        {
+                            foreach (DMScienceScenario.DMScienceData DMData in DMScienceScenario.recoveredScienceList)
+                            {
+                                if (DMData.id == sub.id)
+                                {
+                                    DMModuleScienceAnimate.submitDMScience(DMData, sub);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private List<ScienceData> TransmissionList(Vessel v)
+        {
+            dataList.Clear();
             magicTransmitter = v.FindPartModulesImplementing<MagicDataTransmitter>().First();
             if (magicTransmitter != null)
             {
                 dataList = magicTransmitter.QueuedData;
             }
+            return dataList;
+        }
+
+        private void TransmissionWatcher(Vessel v)
+        {
             foreach (ScienceData data in dataList)
             {
                 foreach (DMScienceScenario.DMScienceData DMData in DMScienceScenario.recoveredScienceList)
